e173751451fa0892c97db875dea89dc86d477107
trivndev <nicolas100107@gmail.com>
2025-08-21 08:04:27 +0700

    add internship information database

---
+++ database/factories/InternshipFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\JobCategory;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Internship>
+ */
+class InternshipFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'job_title' => fake()->jobTitle(),
+            'company' => fake()->company(),
+            'location' => fake()->address(),
+            'description' => fake()->paragraph(2),
+            'requirements' => fake()->paragraph(2),
+            'benefits' => fake()->paragraph(2),
+            'contact_email' => fake()->email(),
+            'contact_phone' => fake()->phoneNumber(),
+            'contact_name' => fake()->name(),
+            'author_id' => User::factory(),
+            'job_category_id' => JobCategory::factory(),
+        ];
+    }
+}
