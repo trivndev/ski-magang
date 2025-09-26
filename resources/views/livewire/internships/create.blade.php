27f073bd529f27b096ed89ae9fc77e7cd525ee0b
trivndev <nicolas100107@gmail.com>
2025-09-26 11:14:37 +0700

    feat(ui): modify create internships filter posts ui

--- resources/views/livewire/internships/create.blade.php
+++ resources/views/livewire/internships/create.blade.php
@@ -6,15 +6,51 @@
                 <flux:button variant="primary">Search</flux:button>
             </div>
         </form>
-        <flux:modal name="hi">
-            Filter
+        <flux:modal name="filter-posts" class="max-w-[90%] w-full sm:max-w-lg outline-none"
+                    :dismissible="false">
+            <div class="space-y-3">
+                <div>
+                    <flux:heading class="text-xl">Filter Posts</flux:heading>
+                </div>
+                <flux:separator/>
+                <form class="space-y-6">
+                    <div class="block md:hidden">
+                        <form>
+                            <div class="flex gap-3 items-center">
+                                <flux:input placeholder="Search post" icon="magnifying-glass"/>
+                                <flux:button variant="primary">Search</flux:button>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <flux:label>
+                            Sort By
+                        </flux:label>
+                        <flux:select placeholder="Default Newest">
+                            <flux:select.option>Newest</flux:select.option>
+                            <flux:select.option>Oldest</flux:select.option>
+                        </flux:select>
+                    </div>
+                    <div>
+                        <flux:checkbox.group label="Select Major">
+                            <flux:checkbox label="TKJ" value="tkj" checked/>
+                            <flux:checkbox label="AKL" value="akl" checked/>
+                            <flux:checkbox label="BID" value="bid" checked/>
+                        </flux:checkbox.group>
+                    </div>
+                    <div class="justify-self-end space-x-1">
+                        <flux:button variant="primary" color="red">Reset</flux:button>
+                        <flux:button variant="primary">Apply</flux:button>
+                    </div>
+                </form>
+            </div>
         </flux:modal>
-        <flux:modal.trigger name="hi" class="flex md:hidden">
+        <flux:modal.trigger name="filter-posts" class="flex md:hidden">
             <flux:button icon="adjustments-horizontal" variant="primary">
                 Filter
             </flux:button>
         </flux:modal.trigger>
-        <flux:modal.trigger name="hi" class="hidden md:flex">
+        <flux:modal.trigger name="filter-posts" class="hidden md:flex">
             <flux:button icon="adjustments-horizontal" variant="primary"/>
         </flux:modal.trigger>
     </div>
