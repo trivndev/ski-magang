65071f59276b25a430a6674c30ebe7a167be1cd5
trivndev <147125501+trivndev@users.noreply.github.com>
2025-10-13 21:57:35 +0700

    feat: add reusable Livewire trait for query filtering and search (#34)
    - Introduced  trait to manage search, filter, and sort states via URL.
    - Added draft state variables to separate temporary user input from active filters.
    - Implemented methods to apply, clear, and sync draft filters with URL state.
    - Ensured pagination reset when filters or search query are updated.
    
    Co-authored-by: Etha <ethanhans11@gmail.com>

--- resources/views/livewire/internships/liked-post.blade.php
+++ resources/views/livewire/internships/liked-post.blade.php
@@ -18,69 +18,19 @@
     </script>
 @endpush
 <div class="space-x-12 space-y-8 mx-auto max-w-7xl py-8 px-4 md:py-16">
-    <div class="flex justify-between w-full">
-        <form class="hidden md:block" wire:click.prevent="searchPost">
-            <div class="flex gap-3 items-center">
-                <flux:input placeholder="Search post" icon="magnifying-glass" class="max-w-xs"
-                            wire:model="searchQuery"/>
-                <flux:button variant="primary" type="submit">Search</flux:button>
-            </div>
-        </form>
-        <flux:modal name="filter-posts" class="max-w-[90%] w-full sm:max-w-lg outline-none"
-                    :dismissible="false">
-            <div class="space-y-3">
-                <div>
-                    <flux:heading class="text-xl">Filter Posts</flux:heading>
-                </div>
-                <flux:separator/>
-                <form class="space-y-6" wire:submit.prevent="applyFilters">
-                    <div class="block md:hidden">
-                        <div class="flex gap-3 items-center">
-                            <flux:input wire:model="searchQuery" placeholder="Search post" icon="magnifying-glass"/>
-                            <flux:button variant="primary">Search</flux:button>
-                        </div>
-                    </div>
-                    <div>
-                        <flux:label>
-                            Sort By
-                        </flux:label>
-                        <flux:select placeholder="Default Newest" wire:model="sortBy">
-                            <flux:select.option value="newest">Newest</flux:select.option>
-                            <flux:select.option value="oldest">Oldest</flux:select.option>
-                            <flux:select.option value="likes">Most liked</flux:select.option>
-                        </flux:select>
-                    </div>
-                    <div>
-                        <flux:checkbox.group label="Select Major" wire:model="selectedMajor">
-                            @php
-                                $vocationalMajors = VocationalMajor::all();
-                            @endphp
-                                @foreach($vocationalMajors as $vocationalMajor)
-                                <flux:checkbox label="{{ $vocationalMajor->major_name }}"
-                                               value="{{ $vocationalMajor->id }}"/>
-                            @endforeach
-                        </flux:checkbox.group>
-                    </div>
-                    <div class="justify-self-end space-x-1">
-                        <flux:button variant="primary" color="red" wire:click="clearFilters">Reset</flux:button>
-                        <flux:button variant="primary" wire:click="applyFilters">Apply</flux:button>
-                    </div>
-                </form>
-            </div>
-        </flux:modal>
-        <flux:modal.trigger name="filter-posts" class="flex md:hidden">
-            <flux:button icon="adjustments-horizontal" variant="primary">
-                Filter
-            </flux:button>
-        </flux:modal.trigger>
-        <flux:modal.trigger name="filter-posts" class="hidden md:flex">
-            <flux:button icon="adjustments-horizontal" variant="primary"/>
-        </flux:modal.trigger>
+    <div class="space-y-4 w-full">
+        <flux:heading class="text-xl">
+            Liked Posts
+        </flux:heading>
+        <x-internship.filter-search/>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 w-full gap-6 md:gap-8">
-        @foreach($internships as $internship)
-            <x-internship.card :$internship/>
-        @endforeach
+    <div data-aos="fade-up" data-aos-duration="500" data-aos-once="true" data-aos-anchor-placement="top-bottom"
+         class="space-y-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 w-full gap-6 md:gap-8">
+            @foreach($internships as $internship)
+                <x-internship.card :$internship/>
+            @endforeach
+        </div>
+        {{ $internships->links() }}
     </div>
-    {{ $internships->links() }}
 </div>
